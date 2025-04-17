a050e64e9611a7a7dc7bacd050f3e23766172943
Alfredo Siguenza <siguenzaalfred@gmail.com>
2025-04-16 21:33:53 -0600

    cambio_1_uploads
    probando la URL de uploads en azure

--- src/main/java/sv/mh/fe/constantes/Constantes.java
+++ src/main/java/sv/mh/fe/constantes/Constantes.java
@@ -3,6 +3,7 @@ package sv.mh.fe.constantes;
 public class Constantes {
 
 	
-	public static final String DIRECTORY_UPLOADS = System.getProperty("user.dir")+"/uploads";	
+	//public static final String DIRECTORY_UPLOADS = System.getProperty("user.dir")+"/uploads";	
+	public static final String DIRECTORY_UPLOADS = "/home/site/wwwroot/uploads";	
 	
 }
