31be73ea59c9c3fb8d61546934c75de01a1baac3
Alfredo Siguenza <siguenzaalfred@gmail.com>
2025-04-19 06:47:50 -0600

    update_original
    update original url uploads

--- src/main/java/sv/mh/fe/constantes/Constantes.java
+++ src/main/java/sv/mh/fe/constantes/Constantes.java
@@ -3,6 +3,6 @@ package sv.mh.fe.constantes;
 public class Constantes {
 
 	
-	public static final String DIRECTORY_UPLOADS =  "/site/wwwroot/uploads/uploads";	
+	public static final String DIRECTORY_UPLOADS = System.getProperty("user.dir")+"/uploads";	
 	
 }
