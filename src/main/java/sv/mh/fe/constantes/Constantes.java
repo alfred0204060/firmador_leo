fee1ef09a994339083f440f8d0d6c7daf454c9e8
Alfredo Siguenza <siguenzaalfred@gmail.com>
2025-06-14 17:04:21 -0600

    ajuste2
    ajuste2

--- src/main/java/sv/mh/fe/constantes/Constantes.java
+++ src/main/java/sv/mh/fe/constantes/Constantes.java
@@ -4,5 +4,5 @@ public class Constantes {
 
 	
 	//public static final String DIRECTORY_UPLOADS = System.getProperty("user.dir")+"/uploads";	
-	public static final String DIRECTORY_UPLOADS = "/home/site/wwwroot/uploads";
+	public static final String DIRECTORY_UPLOADS = "wwwroot/uploads";
 }
